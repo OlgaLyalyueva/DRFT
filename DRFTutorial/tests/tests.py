38eeaa105cf018197de6b35adcf64c0f7d199cda
OlgaLyalyueva <olgalyalyueva@gmail.com>
2023-05-03 15:57:10 +0300

    create test DRF

---
+++ DRFTutorial/tests/tests.py
@@ -0,0 +1,75 @@
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+
+from DRFTutorial.models import Snippet
+
+c = APIClient()
+
+
+class GetListUsers(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create(username='TestUser1',
+                            email='test@tset.test')
+        user.set_password('1234567890')
+        user.save()
+
+    def test_get_user_list(self):
+        response = c.get('/users/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_count_users(self):
+        response = c.get('/users/')
+        self.assertEqual(response.json()['count'], 1)
+
+    def test_user_details(self):
+        response = c.get('/users/')
+        self.assertContains(response, 'TestUser1')
+        self.assertEqual(len(response.json()['results'][0]['snippets']), 0)
+
+
+class GetListSnippets(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create(username='User1')
+        user1.set_password('1234567890')
+        user1.save()
+
+        user2 = User.objects.create(username='User2')
+        user2.set_password('12345678901')
+        user2.save()
+
+        Snippet.objects.create(title='test title1',
+                               linenos=True,
+                               code='Code1',
+                               owner_id=user1.id
+                               )
+        Snippet.objects.create(code='Code2', owner_id=user2.id)
+
+    def test_check_url_unauthorized_user(self):
+        response = c.get('/snippets/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_list_snippets_unauthorized_user(self):
+        response = c.get('/snippets/')
+        self.assertEqual(response.json()['count'], 2)
+
+    def test_get_list_snippets_authorized_user(self):
+        response = c.get('/snippets/')
+        res = c.login(username='User1', password='1234567890')
+        self.assertTrue(res)
+        self.assertEqual(response.json()['count'], 2)
+
+    def test_snippets_details_unauthorized_user(self):
+        user = User.objects.get(id=1)
+        response = c.get('/snippets/')
+        self.assertEqual(response.json()['results'][0]['title'], 'test title1')
+        self.assertEqual(response.json()['results'][0]['code'], 'Code1')
+        self.assertEqual(response.json()['results'][0]['linenos'], True)
+        self.assertEqual(response.json()['results'][0]['language'], 'python')
+        self.assertEqual(response.json()['results'][0]['style'], 'friendly')
+        self.assertEqual(response.json()['results'][0]['owner'], user.username)
---
+++ DRFTutorial/urls.py
@@ -0,0 +1,92 @@
+"""
+URL configuration for DRFTutorial project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from DRFTutorial import views
+
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+''' for ViewSet class but with  фт extended route '''
+# from django.urls import path
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from rest_framework import renderers
+# from DRFTutorial.views import api_root, SnippetViewSet, UserViewSet
+#
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+#
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+#
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+#
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+#
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+#
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
+
+
+''' from first to fourth options'''
+# from django.urls import path, include
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from DRFTutorial import views
+#
+#
+# urlpatterns = [
+#     path('', views.api_root),
+#     path('snippets/', views.SnippetList.as_view()),
+#     path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+#     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+#     path('users/', views.UserList.as_view()),
+#     path('user/<int:pk>/', views.UserDetail.as_view()),
+# ]
+#
+# urlpatterns += [
+#     path('api-auth/', include('rest_framework.urls')),
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
